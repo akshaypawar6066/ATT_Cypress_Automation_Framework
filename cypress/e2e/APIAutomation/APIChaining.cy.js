b13f4b31680acf72ea9a7652d30424b67d12755d
akshaypawar6066 <akshaypawar6066@gmail.com>
2023-12-07 01:53:05 +0530

    Added GitHub workflows and actions

---
+++ cypress/e2e/APIAutomation/APIChaining.cy.js
@@ -0,0 +1,71 @@
+const { describe } = require("mocha");
+
+describe('API Chanining in cypress', () => {
+    let accessToken;
+    let baseUrl = "https://simple-books-api.glitch.me";
+    let bookOrderId;
+    it('Post-Call to get access token', () => {
+        cy.request(
+            {
+                method: 'POST',
+                url: `${baseUrl}/api-clients/`,
+                body: {
+                    "clientName": "Test",
+                    "clientEmail": `testUser${Math.random() * 100}@gmail.com`
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+
+                }
+            }
+        )
+            .then((response) => {
+                expect(response.status).to.equal(201);
+                accessToken = response.body.accessToken
+                cy.log("Access token is:" + accessToken)
+            })
+    });
+
+    it('Post-Call Submit Book Order', () => {
+        cy.request(
+            {
+                method: 'POST',
+                url: `${baseUrl}/orders/`,
+                body: {
+                    "bookId": "5",
+                    "customerName": "Akshay"
+                },
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + accessToken
+                }
+
+            }
+        )
+            .then((response) => {
+                expect(response.status).to.equal(201);
+                expect(response.body.created).to.equal(true);
+                bookOrderId = response.body.orderId;
+                cy.log('Ordered Book Id is:' + bookOrderId)
+            })
+    });
+
+    it('Get-Call Get the details of oredred book', () => {
+        cy.request(
+            {
+                method: 'GET',
+                url: `${baseUrl}/orders/${bookOrderId}`,
+                headers: {
+                    'Authorization': 'Bearer ' + accessToken
+                }
+
+            }
+        )
+            .then((response) => {
+                expect(response.status).to.equal(200);
+                expect(response.body.id).to.equal(bookOrderId);
+                expect(response.body.bookId).to.equal(5);
+                expect(response.body.customerName).to.equal('Akshay');
+            })
+    })
+})
---
+++ cypress/e2e/Intercept/APIIntercept.cy.js
@@ -0,0 +1,74 @@
+describe('Intercept In Cypress', () => {
+   let resBody;
+    before('Getting Body', ()=>
+    {
+        cy.log('Fetching body for request:')
+       cy.fixture('intercept.json').then((data)=>
+       {
+        resBody=data;
+       })
+    })
+    it('Test API With simple intercept', () => {
+        cy.visit('https://dummyapi.io/explorer');
+        cy.intercept(
+            {
+                path: '/data/v1/post/60d21af267d0d8992e610b8d/comment?limit=10'
+            }
+        ).as('comments');
+
+        cy.xpath("//div[text()='Comments List']").click();
+        cy.wait('@comments').then((interceptResponse) => {
+            cy.log(JSON.stringify(interceptResponse));
+        })
+    })
+
+
+    it('Test API With simple intercept and create stub for response', () => {
+        cy.visit('https://dummyapi.io/explorer');
+        cy.intercept(
+            {
+                method: 'GET',
+                url: '/data/v1/post/60d21af267d0d8992e610b8d/comment?limit=10'
+
+
+            },
+            {
+                statusCode: 200,
+                body: { message: 'This is stubbed response', createdBy: 'Akshay' }
+            }
+        ).as('comments');
+        cy.xpath("//div[text()='Comments List']").click();
+        cy.wait('@comments').then((interceptResponse) => {
+            cy.log(JSON.stringify(interceptResponse));
+            console.log(JSON.stringify(interceptResponse));
+            expect(interceptResponse.response.body.createdBy).to.equal('Akshay');
+            expect(interceptResponse.response.statusCode).to.equal(200);
+
+        })
+    })
+
+
+    it('Test API With simple intercept and create stub for response using fixture', () => {
+        cy.visit('https://dummyapi.io/explorer');
+        cy.intercept(
+            {
+                method: 'GET',
+                url: '/data/v1/post/60d21af267d0d8992e610b8d/comment?limit=10'
+
+
+            },
+            {
+                statusCode: 200,
+                body:resBody
+            }
+        ).as('comments');
+        cy.xpath("//div[text()='Comments List']").click();
+        cy.wait('@comments').then((interceptResponse) => {
+            cy.log(JSON.stringify(interceptResponse));
+            console.log(JSON.stringify(interceptResponse));
+            expect(interceptResponse.response.body.createdBy).to.equal('Akshay');
+            expect(interceptResponse.response.statusCode).to.equal(200);
+
+        })
+    })
+})
